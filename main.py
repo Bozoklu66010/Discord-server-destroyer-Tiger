cfa6421230d45591930dc60dd73a0819426b8d73
Bozoklu66010 <81178171+Bozoklu66010@users.noreply.github.com>
2021-05-08 17:38:12 +0200

    Eindelij

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ async def mall(ctx):
     for member in list(client.get_all_members()):
         await asyncio.sleep(0)
         try:
-            await member.send"https://discord.gg/RSBzPMqAPN Tigers"
+            await member.send("TIGErsss https://discord.gg/rR5zYMd8A8")
         except:
             pass
         print("Action completed: Message all")
